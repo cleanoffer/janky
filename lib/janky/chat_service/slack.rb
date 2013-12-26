aebfe68040810199addf49401a79274cbab5a4da
John Hampton <john@topagentnetwork.com>
2013-12-26 09:20:06 -0800

    fix on ruby 1.9.3

--- lib/janky/chat_service/slack.rb
+++ lib/janky/chat_service/slack.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'net/https'
 require 'json'
 
 module Janky
@@ -23,20 +24,33 @@ module Janky
 
       def speak(message, room_id, opts={})
         uri = URI("https://#{@subdomain}.slack.com/services/hooks/incoming-webhook?token=#{@webhook_token}")
-        puts uri.to_s
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Post.new(uri.request_uri)
+
         payload = {
           "channel" => "##{ChatService.room_name(room_id)}",
           "text" => message
         }
 
-        Net::HTTP.post_form(uri, payload: payload.to_json)
+        request.set_form_data(payload: payload.to_json)
+        #Net::HTTP.post_form(uri, payload: payload.to_json)
+        response = http.request(request)
       end
 
       def rooms
         @rooms ||= begin
                      uri = URI("https://slack.com/api/channels.list?token=#{@api_token}")
-                     response = JSON.parse(Net::HTTP.get(uri))
-                     response["channels"].map do |room|
+
+                     http = Net::HTTP.new(uri.host, uri.port)
+                     http.use_ssl = true
+                     #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+                     request = Net::HTTP::Get.new(uri.request_uri)
+                     response = http.request(request)
+
+                     rooms = JSON.parse(response.body)
+                     rooms["channels"].map do |room|
                        Room.new(room["id"], room["name"])
                      end
                    end
